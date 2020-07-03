0c74d7d59828c7c0bdc12b485a5d359c9233e448
Jitendra Ojha <jitendraojha1217@gmail.com>
2020-07-03 07:44:00 +0530

    Adds generated files

--- example/generated/Twitch/Twirp/Example/HaberdasherClient.php
+++ example/generated/Twitch/Twirp/Example/HaberdasherClient.php
@@ -8,16 +8,6 @@ namespace Twitch\Twirp\Example;
 
 use Google\Protobuf\Internal\GPBDecodeException;
 use Google\Protobuf\Internal\Message;
-use Http\Discovery\Psr17FactoryDiscovery;
-use Http\Discovery\Psr18ClientDiscovery;
-use Psr\Http\Client\ClientInterface;
-use Psr\Http\Message\RequestFactoryInterface;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Twirp\Context;
-use Twirp\Error;
-use Twirp\ErrorCode;
 
 /**
  * A Protobuf client that implements the {@see Haberdasher} interface.
@@ -25,74 +15,12 @@ use Twirp\ErrorCode;
  *
  * Generated from protobuf service <code>twitch.twirp.example.Haberdasher</code>
  */
-final class HaberdasherClient implements Haberdasher
+final class HaberdasherClient extends HaberdasherAbstractClient implements Haberdasher
 {
     /**
-     * @var server
+     * @inheritDoc
      */
-    private $addr;
-
-    /**
-     * @var ClientInterface
-     */
-    private $httpClient;
-
-    /**
-     * @var RequestFactoryInterface
-     */
-    private $requestFactory;
-
-    /**
-     * @var StreamFactoryInterface
-     */
-    private $streamFactory;
-
-    public function __construct(
-        $addr,
-        ClientInterface $httpClient = null,
-        RequestFactoryInterface $requestFactory = null,
-        StreamFactoryInterface $streamFactory = null
-    ) {
-        if ($httpClient === null) {
-            $httpClient = Psr18ClientDiscovery::find();
-        }
-
-        if ($requestFactory === null) {
-            $requestFactory = Psr17FactoryDiscovery::findRequestFactory();
-        }
-
-        if ($streamFactory === null) {
-            $streamFactory = Psr17FactoryDiscovery::findStreamFactory();
-        }
-
-        $this->addr = $this->urlBase($addr);
-        $this->httpClient = $httpClient;
-        $this->requestFactory = $requestFactory;
-        $this->streamFactory = $streamFactory;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function MakeHat(array $ctx, \Twitch\Twirp\Example\Size $in): \Twitch\Twirp\Example\Hat
-    {
-        $ctx = Context::withPackageName($ctx, 'twitch.twirp.example');
-        $ctx = Context::withServiceName($ctx, 'Haberdasher');
-        $ctx = Context::withMethodName($ctx, 'MakeHat');
-
-        $out = new \Twitch\Twirp\Example\Hat();
-
-        $url = $this->addr.'/twirp/twitch.twirp.example.Haberdasher/MakeHat';
-
-        $this->doProtobufRequest($ctx, $url, $in, $out);
-
-        return $out;
-    }
-
-    /**
-     * Common code to make a request to the remote twirp service.
-     */
-    private function doProtobufRequest(array $ctx, string $url, Message $in, Message $out): void
+    protected function doRequest(array $ctx, string $url, Message $in, Message $out): void
     {
         $body = $in->serializeToString();
 
@@ -113,157 +41,5 @@ final class HaberdasherClient implements Haberdasher
         } catch (GPBDecodeException $e) {
             throw $this->clientError('failed to unmarshal proto response', $e);
         }
-    }
-
-    /**
-     * Makes an HTTP request and adds common headers.
-     */
-    private function newRequest(array $ctx, string $url, string $reqBody, string $contentType): RequestInterface
-    {
-        $body = $this->streamFactory->createStream($reqBody);
-
-        $req = $this->requestFactory->createRequest('POST', $url);
-
-        $headers = Context::httpRequestHeaders($ctx);
-
-        foreach ($headers as $key => $value) {
-            $req = $req->withHeader($key, $value);
-        }
-
-        return $req
-            ->withBody($body)
-            ->withHeader('Accept', $contentType)
-            ->withHeader('Content-Type', $contentType)
-            ->withHeader('Twirp-Version', 'v5.3.0');
-    }
-
-    /**
-     * Adds consistency to errors generated in the client.
-     */
-    private function clientError(string $desc, \Throwable $e): TwirpError
-    {
-        return TwirpError::newError(ErrorCode::Internal, sprintf('%s: %s', $desc, $e->getMessage()));
-    }
-
-    /**
-     * Builds a twirp Error from a non-200 HTTP response.
-     * If the response has a valid serialized Twirp error, then it's returned.
-     * If not, the response status code is used to generate a similar twirp
-     * error. {@see self::twirpErrorFromIntermediary} for more info on intermediary errors.
-     */
-    private function errorFromResponse(ResponseInterface $resp): TwirpError
-    {
-        $statusCode = $resp->getStatusCode();
-        $statusText = $resp->getReasonPhrase();
-
-        if ($this->isHttpRedirect($statusCode)) {
-            // Unexpected redirect: it must be an error from an intermediary.
-            // Twirp clients don't follow redirects automatically, Twirp only handles
-            // POST requests, redirects should only happen on GET and HEAD requests.
-            $location = $resp->getHeaderLine('Location');
-            $msg = sprintf(
-                'unexpected HTTP status code %d "%s" received, Location="%s"',
-                $statusCode,
-                $statusText,
-                $location
-            );
-
-            return $this->twirpErrorFromIntermediary($statusCode, $msg, $location);
-        }
-
-        $body = (string)$resp->getBody();
-
-        $rawError = json_decode($body, true);
-        if ($rawError === null) {
-            $msg = sprintf('error from intermediary with HTTP status code %d "%s"', $statusCode, $statusText);
-
-            return $this->twirpErrorFromIntermediary($statusCode, $msg, $body);
-        }
-
-        $rawError = $rawError + ['code' => '', 'msg' => '', 'meta' => []];
-
-        if (ErrorCode::isValid($rawError['code']) === false) {
-            $msg = 'invalid type returned from server error response: '.$rawError['code'];
-
-            return TwirpError::newError(ErrorCode::Internal, $msg);
-        }
-
-        $error = TwirpError::newError($rawError['code'], $rawError['msg']);
-
-        foreach ($rawError['meta'] as $key => $value) {
-           $error->setMeta($key, $value);
-        }
-
-        return $error;
-    }
-
-    /**
-     * Maps HTTP errors from non-twirp sources to twirp errors.
-     * The mapping is similar to gRPC: https://github.com/grpc/grpc/blob/master/doc/http-grpc-status-mapping.md.
-     * Returned twirp Errors have some additional metadata for inspection.
-     */
-    private function twirpErrorFromIntermediary(int $status, string $msg, string $bodyOrLocation): TwirpError
-    {
-        if ($this->isHttpRedirect($status)) {
-            $code = ErrorCode::Internal;
-        } else {
-            switch ($status) {
-                case 400: // Bad Request
-                    $code = ErrorCode::Internal;
-                    break;
-                case 401: // Unauthorized
-                    $code = ErrorCode::Unauthenticated;
-                    break;
-                case 403: // Forbidden
-                    $code = ErrorCode::PermissionDenied;
-                    break;
-                case 404: // Not Found
-                    $code = ErrorCode::BadRoute;
-                    break;
-                case 429: // Too Many Requests
-                case 502: // Bad Gateway
-                case 503: // Service Unavailable
-                case 504: // Gateway Timeout
-                    $code = ErrorCode::Unavailable;
-                    break;
-                default: // All other codes
-                    $code = ErrorCode::Unknown;
-                    break;
-            }
-        }
-
-        $error = TwirpError::newError($code, $msg);
-        $error->setMeta('http_error_from_intermediary', 'true');
-        $error->setMeta('status_code', (string)$status);
-
-        if ($this->isHttpRedirect($status)) {
-            $error->setMeta('location', $bodyOrLocation);
-        } else {
-            $error->setMeta('body', $bodyOrLocation);
-        }
-
-        return $error;
-    }
-
-    private function isHttpRedirect(int $status): bool
-    {
-        return $status >= 300 && $status <= 399;
-    }
-
-    private function urlBase(string $addr): string
-    {
-        $scheme = parse_url($addr, PHP_URL_SCHEME);
-
-        // If parse_url fails, return the addr unchanged.
-        if ($scheme === false) {
-            return $addr;
-        }
-
-        // If the addr does not specify a scheme, default to http.
-        if (empty($scheme)) {
-            $addr = 'http://'.ltrim($addr, ':/');
-        }
-
-        return $addr;
     }
 }
