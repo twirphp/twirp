2f69fc49951d97ec1a595b1f6bdf8ea57ac13a17
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-04-21 13:13:45 +0200

    feat: ignore unkown fields
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- protoc-gen-twirp_php/templates/service/Server.php
+++ protoc-gen-twirp_php/templates/service/Server.php
@@ -146,7 +146,7 @@ final class {{ .Service | phpServiceName .File }}Server implements RequestHandle
             $ctx = $this->hook->requestRouted($ctx);
 
             $in = new {{ $inputType }}();
-            $in->mergeFromJsonString((string)$req->getBody());
+            $in->mergeFromJsonString((string)$req->getBody(), true);
 
             $out = $this->svc->{{ $method.Desc.Name }}($ctx, $in);
 
