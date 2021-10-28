f8e5c871874cc38ec532330ff421d716b33909f5
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-10-29 00:19:37 +0200

    build: add nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Twirp PHP port";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            gnumake
+            go
+            php
+            protobuf
+            php.packages.composer
+            golangci-lint
+            gotestsum
+            goreleaser
+          ];
+        };
+      });
+}
