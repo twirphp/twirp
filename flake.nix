a4880aae1bbfb31c2339bf18ce2095fb2e03c72c
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2021-10-29 03:31:43 +0200

    build: add clientcompat to nix
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -12,20 +12,37 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        clientcompat = pkgs.buildGoPackage rec {
+          pname = "clientcompat";
+          version = "5.12.1";
+
+          goPackagePath = "github.com/twitchtv/twirp";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "twitchtv";
+            repo = "twirp";
+            rev = "v${version}";
+            sha256 = "OXBeaoANUjh+s5baO0mw4zyVAFtn+VHMZgfxVNuUvAI=";
+          };
+
+          subPackages = [ "clientcompat" ];
+        };
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            git
-            gnumake
-            go
-            php
-            protobuf
-            php.packages.composer
-            golangci-lint
-            gotestsum
-            goreleaser
-          ];
+          buildInputs = with pkgs;
+            [
+              git
+              gnumake
+              go
+              php
+              protobuf
+              php.packages.composer
+              golangci-lint
+              gotestsum
+              goreleaser
+            ] ++ [ clientcompat ];
         };
       });
 }
