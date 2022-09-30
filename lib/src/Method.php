175844f9ec6ec59b92d7acdac8e5bc1bd52c90e6
Andreas Linden <andreas.linden@coxautoinc.com>
2022-09-30 08:19:21 +0200

    Change Interceptor Method to an actual class. Run interceptor before making a request.

--- lib/src/Method.php
+++ lib/src/Method.php
@@ -4,17 +4,63 @@ declare(strict_types=1);
 
 namespace Twirp;
 
+use Psr\Http\Message\RequestInterface;
+
 /**
  * Method is a generic representation of a Twirp-generated RPC method.
  *
  * It is used to define Interceptors.
  */
-interface Method
+class Method
 {
     /**
-     * Generic representation of the underlying method call.
-     *
-     * Since PHP doesn't support function types, it is defined as an interface.
+     * @var array
      */
-    public function call(array $ctx, object $request): object;
+    protected $ctx;
+
+    /**
+     * @var RequestInterface
+     */
+    protected $request;
+
+    /**
+     * Generic representation of the underlying method call.
+     */
+    public function __construct(array $ctx, RequestInterface $request)
+    {
+        $this->ctx = $ctx;
+        $this->request = $request;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCtx(): array
+    {
+        return $this->ctx;
+    }
+
+    /**
+     * @param array $ctx
+     */
+    public function setCtx(array $ctx): void
+    {
+        $this->ctx = $ctx;
+    }
+
+    /**
+     * @return RequestInterface
+     */
+    public function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param RequestInterface $request
+     */
+    public function setRequest(RequestInterface $request): void
+    {
+        $this->request = $request;
+    }
 }
