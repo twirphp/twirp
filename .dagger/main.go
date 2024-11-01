b75b81df96e8385c8de6100114dd89d781a603be
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-11-01 11:15:07 +0100

    add dagger
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ .dagger/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"dagger/twirp/internal/dagger"
+	"fmt"
+)
+
+type Twirp struct {
+	// Project source directory
+	// This will become useful once pulling from remote becomes available
+	//
+	// +private
+	Source *dagger.Directory
+}
+
+func New(
+	// Project source directory.
+	//
+	// +defaultPath=/
+	// +ignore=[".devenv", ".direnv", ".github", "build", ".pre-commit-config.yaml"]
+	source *dagger.Directory,
+) (*Twirp, error) {
+	return &Twirp{
+		Source: source,
+	}, nil
+}
+
+func (m *Twirp) Test() *dagger.Container {
+	return dag.Container().From("alpine").WithWorkdir("/work").WithMountedDirectory("/work", m.Source).Terminal()
+}
+
+// Build clientcompat binary.
+func (m *Twirp) Clientcompat(
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	//
+	// +optional
+	platform dagger.Platform,
+) *dagger.File {
+	return dag.Go(dagger.GoOpts{
+		Version: goVersion,
+	}).
+		Exec([]string{"go", "install", fmt.Sprintf("github.com/twitchtv/twirp/clientcompat@%s", twirpVersion)}).
+		File("/go/bin/clientcompat")
+}
+
+// Build clientcompat binary.
+func (m *Twirp) Build(
+	// Target platform in "[os]/[platform]/[version]" format (e.g., "darwin/arm64/v7", "windows/amd64", "linux/arm64").
+	//
+	// +optional
+	platform dagger.Platform,
+) *dagger.File {
+	return m.build(platform, "dev")
+}
+
+func (m *Twirp) build(platform dagger.Platform, version string) *dagger.File {
+	if version == "" {
+		version = "unknown"
+	}
+
+	return dag.Go(dagger.GoOpts{
+		Version: goVersion,
+	}).
+		WithSource(m.Source).
+		Build(dagger.GoWithSourceBuildOpts{
+			Pkg:      "./protoc-gen-twirp_php",
+			Trimpath: true,
+			Ldflags: []string{
+				"-s", "-w",
+				"-X", "main.version=" + version,
+			},
+			Platform: platform,
+		})
+}
+
+func (m *Twirp) protoc() *dagger.Container {
+	return dag.Container().
+		From("")
+}
---
+++ .dagger/versions_pinned.go
@@ -0,0 +1,7 @@
+package main
+
+const (
+	goVersion = "1.23.0"
+
+	twirpVersion = "v8.1.3"
+)
